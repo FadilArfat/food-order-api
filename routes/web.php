a678cfe0267a18830180bd9a524635b710125c63
FadilArfat <fadilitas14@gmail.com>
2025-03-18 19:28:23 +0700

    second commit3

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 
 });
+Route::get('/create-symlink', function (){
+    symlink(storage_path('/app/public'), public_path('storage'));
+    echo "Symlink Created. Thanks";
+});
